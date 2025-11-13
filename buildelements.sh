27cd21dc4924a4080b93e60035ce883c71551c77
Byron Hambly <bhambly@blockstream.com>
2025-11-13 09:12:30 +0200

    wip

--- buildelements.sh
+++ buildelements.sh
@@ -5,7 +5,9 @@
 ## cd core-review/guix/
 ## DOCKER_BUILDKIT=1 docker build --pull --no-cache -t alpine-guix - < Dockerfile
 
-export ELEMENTS_SRC="$PWD/elements/"
+export ELEMENTS_SRC="/home/byron/code/elements/"
+export BUILD_TAG="elements-23.3.1rc3"
+#export HOST="x86_64-linux-gnu"
 
 set -e
 
@@ -27,12 +29,12 @@ echo "tagbuild: ${tagbuild}"
 builddir="guix-build-${tagbuild#v}"
 echo "builddir: ${builddir}"
 
-echo "host: $HOST"
-NAME=${HOST//-/_}
-echo "name: $NAME"
+#echo "host: $HOST"
+#NAME=${HOST//-/_}
+#echo "name: $NAME"
 
+MACOS_SDK="Xcode-12.2-12B45b-extracted-SDK-with-libcxx-headers"
 echo "macos sdk: $MACOS_SDK"
-
 
 cat >tmpelementsbuild.sh <<__EOF__
 #!/bin/bash
@@ -44,13 +46,13 @@ cd /elements
 export SOURCES_PATH=/sources
 export BASE_CACHE=/base_cache
 
-export HOSTS="$HOST"
-echo $HOST
-echo $NAME
+#export HOSTS="$HOST"
+#echo $HOST
+#echo $NAME
 
 ./contrib/guix/guix-clean
 
-if [[ $HOST == *"apple"* ]];then
+#if [[ $HOST == *"apple"* ]];then
     if [ ! -d /elements/depends/SDKs/$MACOS_SDK ];then
         mkdir -p /elements/depends/SDKs/
         pushd /elements/depends/SDKs/
@@ -58,22 +60,23 @@ if [[ $HOST == *"apple"* ]];then
         tar -xf /sources/$MACOS_SDK.tar.gz
         popd
     fi
-fi
+#fi
 
 export FORCE_DIRTY_WORKTREE=true
 time ./contrib/guix/guix-build
-pwd
-ls -alht
-echo $builddir
-ls -alht $builddir
-ls -alht $builddir/output/
-find $builddir/output/ -type f -print0 | env LC_ALL=C sort -z | xargs -r0 sha256sum | tee $NAME.txt
-mv $NAME.txt $builddir/output/$NAME.txt
+#pwd
+#ls -alht
+#echo $builddir
+#ls -alht $builddir
+#ls -alht $builddir/output/
+#find $builddir/output/ -type f -print0 | env LC_ALL=C sort -z | xargs -r0 sha256sum | tee $NAME.txt
+#mv $NAME.txt $builddir/output/$NAME.txt
 __EOF__
 
 chmod 700 tmpelementsbuild.sh
 docker cp tmpelementsbuild.sh elementsbuild:/root/elementsbuild.sh
 docker cp sources/. elementsbuild:/sources/
 docker exec -i elementsbuild /root/elementsbuild.sh
-mkdir -p output/
-docker cp elementsbuild:/elements/"$builddir"/output/ output/
+mkdir -p "$builddir"/
+docker cp elementsbuild:/elements/"$builddir"/output/ "$builddir"/
+find $builddir/output/ -type f -print0 | env LC_ALL=C sort -z | xargs -r0 sha256sum
